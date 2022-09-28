d6f7dd474056e25d123f6e97931e614200472a4e
Nathanael Ribeiro <ribeirotomas1904@gmail.com>
2022-09-28 00:11:36 -0300

    Add system f types

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,25 @@ module Main (main) where
 
 import Lib
 
+type Env = [(String, Value)]
+
+data Type_
+    = T_Unit
+    | T_Arrow Type_ Type_
+    | T_Forall String Type_
+
+data Expression
+    = E_Unit
+    | E_Variable String
+    | E_Abstraction String Type_ Expression
+    | E_Application Expression Expression
+    | E_TypeAbstraction String Expression
+    | E_TypeApplication Expression Type_
+
+data Value
+    = V_Unit
+    | V_Closure String Type_ Expression Env
+    | V_NativeFunction Value Value
+
 main :: IO ()
 main = someFunc
