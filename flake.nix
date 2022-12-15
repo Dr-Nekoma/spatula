70638a7bb92e4ee64436f412407d173816ce9855
Marcos Benevides <marcos.schonfinkel@gmail.com>
2022-12-14 23:59:50 -0300

    nix: add proper nix support

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { flake-utils, nixpkgs, self, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        config = {};
+        overlays = [];
+        pkgs = import nixpkgs { inherit config overlays system; };
+        haskellPackages = pkgs.haskellPackages;
+        packageName = "spatula";
+      in rec {
+        packages.${packageName} = # (ref:haskell-package-def)
+          pkgs.haskell.lib.dontCheck (
+            haskellPackages.callCabal2nix packageName self rec {
+              # Dependency overrides go here
+            }
+          );
+
+        defaultPackage = self.packages.${system}.${packageName};
+
+        devShell = haskellPackages.shellFor {
+          inputsFrom = builtins.attrValues self.packages.${system};
+          withHoogle = true;
+
+          packages = p: [
+          ];
+
+          buildInputs = with haskellPackages; [
+            cabal-install
+            stack
+
+            # Extra dev tools
+            ghcid
+            haskell-language-server
+            hlint
+            ormolu
+          ];
+        };
+      }
+    );
+}
